860ad7be3bf92f09c919e02cb243e651a70035e7
Lucas Javaudin <lucas@lucasjavaudin.com>
2025-01-17 16:45:32 +0100

    add real time collect

---
+++ real_time/global_request.py
@@ -0,0 +1,86 @@
+import os
+import shutil
+from datetime import datetime, UTC
+
+import pytz
+import requests
+import polars as pl
+
+API_KEY = "oZ7bT3br86bBxHrRZheood1013nRRnmq"
+
+OUTPUT_FILE = "./rera_times.parquet"
+BACKUP_FILE = "./rera_times.parquet.backup"
+
+CLH = "STIF:StopPoint:Q:40918:"
+# Both directions in the global request, with about 30min of history.
+CP = "STIF:StopPoint:Q:411352:"
+NANTERRE = "STIF:StopPoint:Q:40916:"
+LA_DEFENSE = "STIF:StopPoint:Q:40951:"
+JOINVILLE = "STIF:StopPoint:Q:40932:"
+BOISSY = "STIF:StopPoint:Q:412802:"
+BOISSYS = ("STIF:StopPoint:Q:412802:", "STIF:StopPoint:Q:473984:", "STIF:StopPoint:Q:473988:", "STIF:StopPoint:Q:473987:")
+SUCY = ("STIF:StopPoint:Q:412803:", "STIF:StopPoint:Q:474007:", "STIF:StopPoint:Q:474010:")
+
+RER_A = "STIF:Line::C01742:"
+
+tz = pytz.timezone("Europe/Paris")
+
+now = datetime.now(UTC)
+print("Running request")
+response = requests.get("https://prim.iledefrance-mobilites.fr/marketplace/estimated-timetable", headers={"apiKey": API_KEY})
+
+stop_times = list()
+
+if response.ok:
+    print("Reading response")
+    data = response.json().get("Siri", dict()).get("ServiceDelivery", dict())
+    response_datetime = datetime.fromisoformat(data["ResponseTimestamp"])
+    timetable = data["EstimatedTimetableDelivery"][0]["EstimatedJourneyVersionFrame"][0]["EstimatedVehicleJourney"]
+    rera = list(filter(lambda t: "C01742" in t.get("LineRef", dict()).get("value"), timetable))
+    for trip in rera:
+        longtrain = trip["VehicleFeatureRef"][0] == "longTrain"
+        journey_ref = trip["DatedVehicleJourneyRef"]["value"]
+        dest_ref = trip["DestinationRef"]["value"]
+        dest_name = trip["DestinationName"][0]["value"]
+        for call in trip["EstimatedCalls"]["EstimatedCall"]:
+            exp_dep_time = call.get("ExpectedDepartureTime")
+            if exp_dep_time and datetime.fromisoformat(exp_dep_time) > now:
+                continue
+            exp_arr_time = call.get("ExpectedArrivalTime")
+            if exp_arr_time and datetime.fromisoformat(exp_arr_time) > now:
+                continue
+            aim_dep_time = call.get("AimedDepartureTime")
+            aim_arr_time = call.get("AimedArrivalTime")
+            stop_ref = call["StopPointRef"]["value"]
+            dep_status = call.get("DepartureStatus")
+            arr_status = call.get("ArrivalStatus")
+            stop_times.append({
+                "journey_ref": journey_ref,
+                "dest_ref": dest_ref,
+                "dest_name": dest_name,
+                "stop_ref": stop_ref,
+                "longtrain": longtrain,
+                "dep_status": dep_status,
+                "arr_status": arr_status,
+                "exp_dep_time": exp_dep_time,
+                "exp_arr_time": exp_arr_time,
+                "aim_dep_time": aim_dep_time,
+                "aim_arr_time": aim_arr_time,
+            })
+
+df = pl.DataFrame(stop_times).with_columns(
+    pl.col("exp_dep_time").str.to_datetime(),
+    pl.col("exp_arr_time").str.to_datetime(),
+    pl.col("aim_dep_time").str.to_datetime(),
+    pl.col("aim_arr_time").str.to_datetime(),
+)
+
+if os.path.isfile(OUTPUT_FILE):
+    shutil.copyfile(OUTPUT_FILE, BACKUP_FILE)
+
+    old_df = pl.scan_parquet(OUTPUT_FILE)
+
+    df = pl.concat((df.lazy(), old_df), how="vertical", rechunk=True).unique(subset=["journey_ref", "stop_ref"], keep="first", maintain_order=True).collect()
+
+print(len(df))
+df.write_parquet(OUTPUT_FILE)
---
+++ real_time/unitary.py
@@ -0,0 +1,101 @@
+from datetime import datetime, UTC
+
+import pytz
+import requests
+import polars as pl
+
+API_URL = "https://prim.iledefrance-mobilites.fr/marketplace"
+API_KEY = "oZ7bT3br86bBxHrRZheood1013nRRnmq"
+
+BOISSYS = ("STIF:StopPoint:Q:412802:", "STIF:StopPoint:Q:473984:", "STIF:StopPoint:Q:473988:", "STIF:StopPoint:Q:473987:")
+
+RER_A = "STIF:Line::C01742:"
+
+df = (
+    pl.scan_parquet(
+        "./perimetre-des-donnees-tr-disponibles-plateforme-idfm.parquet"
+    )
+    .filter(pl.col("line") == RER_A)
+    .select("ns2_stoppointref", "ns2_stopname")
+    .collect()
+)
+
+stop_times = list()
+
+for stop_ref, stop_name in zip(df["ns2_stoppointref"], df["ns2_stopname"]):
+    print(f"=== {stop_name} ===")
+    now = datetime.now(UTC)
+    params = {
+        "MonitoringRef": stop_ref,
+        "LineRef": RER_A,
+    }
+    response = requests.get(
+        f"{API_URL}/stop-monitoring",
+        headers={"apiKey": API_KEY},
+        params=params,
+    )
+    if response.ok:
+        timetable = (
+            response.json()
+            .get("Siri", dict())
+            .get("ServiceDelivery", dict())
+            .get("StopMonitoringDelivery", list())[0]
+            .get("MonitoredStopVisit")
+        )
+        if timetable is None:
+            print("Invalid response")
+        for record in timetable:
+            if not "MonitoredCall" in record.get("MonitoredVehicleJourney", dict()):
+                print(record)
+                continue
+            data = record["MonitoredVehicleJourney"]
+            exp_dep_time = data["MonitoredCall"].get("ExpectedDepartureTime")
+            exp_arr_time = data["MonitoredCall"].get("ExpectedArrivalTime")
+            past_dep_time = (
+                bool(exp_dep_time)
+                and datetime.fromisoformat(exp_dep_time) < now
+            )
+            past_arr_time = (
+                bool(exp_arr_time)
+                and datetime.fromisoformat(exp_arr_time) < now
+            )
+            if past_dep_time or (past_arr_time and exp_dep_time is None):
+                aim_dep_time = data["MonitoredCall"].get("AimedDepartureTime")
+                aim_arr_time = data["MonitoredCall"].get("AimedArrivalTime")
+                dep_status = data["MonitoredCall"].get("DepartureStatus")
+                arr_status = data["MonitoredCall"].get("ArrivalStatus")
+                order = data["MonitoredCall"].get("Order")
+                dest_ref = data["DestinationRef"]["value"]
+                dest_name = data["DestinationName"][0]["value"]
+                line = data["LineRef"]["value"]
+                operator = data["OperatorRef"]["value"]
+                long_train = data["VehicleFeatureRef"][0] == "longTrain"
+                stop_times.append({
+                    "line": line,
+                    "stop_ref": stop_ref,
+                    "stop_name": stop_name,
+                    "operator": operator,
+                    "arr_status": arr_status,
+                    "dep_status": dep_status,
+                    "aim_arr_time": aim_arr_time,
+                    "aim_dep_time": aim_dep_time,
+                    "exp_arr_time": exp_arr_time,
+                    "exp_dep_time": exp_dep_time,
+                    "order": order,
+                    "dest_ref": dest_ref,
+                    "dest_name": dest_name,
+                    "long_train": long_train,
+                })
+    else:
+        print(f"Code: {response.code}")
+        print(f"Reason: {response.reason}")
+
+stop_times_df = (
+    pl.DataFrame(stop_times)
+    .with_columns(
+        pl.col("aim_arr_time").str.to_datetime(),
+        pl.col("aim_dep_time").str.to_datetime(),
+        pl.col("exp_arr_time").str.to_datetime(),
+        pl.col("exp_dep_time").str.to_datetime(),
+    )
+)
